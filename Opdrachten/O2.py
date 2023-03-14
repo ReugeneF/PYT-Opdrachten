49fcf3fb3a3d4113c04c7bdf904be7b8579e9858
ReugeneF <reugene_business@hotmail.com>
2023-03-14 11:27:10 +0100

    O2 af

---
+++ Opdrachten/O2.py
@@ -0,0 +1,17 @@
+print("Number even or odd calculator")
+num1 = int(input("Enter a number: "))%4
+if num1%2 == 0:
+    print("Het getal is even")
+else:
+    print("Het getal is oneven")
+
+if num1%4 == 0:
+    print("Het getal is vermedigvuldiging van 4")
+
+num2 = int(input("Number1: "))
+num3 = int(input("Number2: "))
+if num2/num3%2 == 4%2:
+    print("It devides evenly")
+else:
+    print("sike")
+
