9897468849dd0ab69799b078fd3357f03e0a1e2d
ReugeneF <reugene_business@hotmail.com>
2023-03-16 11:10:43 +0100

    O3

--- Opdrachten/O3.py
+++ Opdrachten/O3.py
@@ -2,6 +2,12 @@ list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 for elements in list:
     if elements <=5:
         print(elements)
-        b = []
-        b.append(elements)
-        print(b)
+
+user1 = int(input("Input number: "))
+
+for elements in list:
+    if elements <= user1:
+        print(elements)
+
+
+        
